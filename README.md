1d02ccafc23301aa1a3438a52d1a1fd735169181
Ashish Kumar <ashish.kumar@zopper.com>
2024-05-17 21:47:53 +0530

    added readme file

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
-# react-long-press-hook
+# 📱 react-long-press-hook
+
+Unlock the power of long-press interactions with `react-long-press-hook`! 🚀 This lightweight and highly customizable React hook lets you easily detect and handle long-press events in your applications. Whether you’re looking to enhance your UI with context menus, drag-and-drop functionality, action confirmations, or interactive tutorials, `react-long-press-hook` has got you covered.
+
+## ✨ Features
+
+- 🔧 **Customizable**: Easily configure thresholds, prevent default behavior, and more.
+- 🪶 **Lightweight**: Minimal impact on your bundle size.
+- 👐 **Accessible**: Works with both touch and mouse events.
+- ⚡ **Efficient**: No unnecessary re-renders or performance hits.
+
+## 📦 Installation
+
+```bash
+npm install react-long-press-hook
+```
+or 
+```bash
+yarn add react-long-press-hook
+```
+
+## 📚 Use Cases
+
+### 📜 Context Menus
+Enable context menus on long-press to provide users with additional options or actions.
+
+### 🖱️ Drag-and-Drop
+Implement drag-and-drop functionality where a long-press initiates the dragging action.
+
+### ✅ Action Confirmations
+Use long-press to confirm actions, such as deleting an item, to prevent accidental taps.
+
+### 🎓 Interactive Tutorials
+Create interactive tutorials that require users to long-press on elements to reveal tips or further instructions.
+
+### 🕹️ Custom Gesture Controls
+Develop custom gesture controls for touch interfaces, enhancing user interaction on mobile devices.
+
+### 🎮 Game Mechanics
+Incorporate long-press actions as part of game mechanics, such as charging a power-up or revealing hidden objects.
+
+## 🚀 Usage
+Here is a basic example of how to use the useLongPress hook in your React application:
+
+```
+import React, { useState } from 'react';
+import { useLongPress } from 'react-long-press-hook';
+
+const App = () => {
+  const [message, setMessage] = useState('');
+
+  const onLongPress = () => {
+    setMessage('Long press detected!');
+  };
+
+  const onPressHold = () => {
+    setMessage('Press and hold in progress...');
+  };
+
+  const onPressRelease = () => {
+    setMessage('Press released');
+  };
+
+  const longPressEvent = useLongPress(onLongPress, {
+    onStart: onPressHold,
+    onFinish: onPressRelease,
+    threshold: 2000, // milliseconds
+    preventDefault: true,
+    cancelOnMove: true,
+  });
+
+  return (
+    <div>
+      <button {...longPressEvent}>Long Press Me</button>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default App;
+
+```
+
+## 📚 API
+
+## `useLongPress(callback, options)`
+
+### Parameters
+- `callback` (`function`): The function to call when a long-press is detected.
+- `options` (`object`): Configuration options for the hook.
+  - `onStart` (`function`): Function to call when the press starts.
+  - `onFinish` (`function`): Function to call when the press ends.
+  - `threshold` (`number`): Time in milliseconds to detect a long-press. Default is 300.
+  - `preventDefault` (`boolean`): Whether to prevent the default context menu on long-press. Default is true.
+  - `cancelOnMove` (`boolean`): Whether to cancel the long-press if the pointer moves. Default is false.
+  - `stopPropagation` (`boolean`): Whether to stop event propagation. Default is false.
+
+### Returns
+`handlers` (`object`): Event handlers to spread on the target element.
+
+## Check Out My Other Packages
+
+Explore more useful packages by visiting [my npm profile](https://www.npmjs.com/~iashish.99). Made with ❤️ by Ashish
+
--- src/customHook/useLongPress.ts
+++ src/customHook/useLongPress.ts
@@ -1,4 +1,105 @@
-import * as React from "react";
+// import { useCallback, useRef  } from "react";
+
+
+// interface LongPressOptions {
+//   onStart?: (event: MouseEvent | TouchEvent) => void;
+//   onFinish?: (event: MouseEvent | TouchEvent) => void;
+//   threshold?: number;
+//   preventDefault?: boolean;
+//   cancelOnMove?: boolean;
+//   stopPropagation?: boolean;
+// }
+
+// const useLongPress = (
+//   callback: (event: MouseEvent | TouchEvent) => void,
+//   {
+//     onStart = () => {},
+//     onFinish = () => {},
+//     threshold = 300,
+//     preventDefault = true,
+//     cancelOnMove = false,
+//     stopPropagation = false,
+//   }: LongPressOptions = {}
+// ) => {
+//   const timerRef = useRef<NodeJS.Timeout | null>(null);
+//   const startX = useRef<number | null>(null);
+//   const startY = useRef<number | null>(null);
+
+//   const start = useCallback(
+//     (event: MouseEvent | TouchEvent) => {
+//       if (preventDefault && event.target) {
+//         (event.target as HTMLElement).addEventListener("contextmenu", (e) =>
+//           e.preventDefault()
+//         );
+//       }
+//       if (stopPropagation) {
+//         event.stopPropagation();
+//       }
+
+//       startX.current =
+//         event instanceof MouseEvent ? event.clientX : event.touches[0].clientX;
+//       startY.current =
+//         event instanceof MouseEvent ? event.clientY : event.touches[0].clientY;
+//       onStart(event);
+
+//       timerRef.current = setTimeout(() => {
+//         callback(event);
+//       }, threshold);
+//     },
+//     [callback, onStart, preventDefault, stopPropagation, threshold]
+//   );
+
+//   const stop = useCallback(
+//     (event: MouseEvent | TouchEvent, shouldTriggerCallback: boolean = true) => {
+//       if (preventDefault && event.target) {
+//         (event.target as HTMLElement).removeEventListener("contextmenu", (e) =>
+//           e.preventDefault()
+//         );
+//       }
+
+//       clearTimeout(timerRef.current as any);
+//       if (shouldTriggerCallback) {
+//         onFinish(event);
+//       }
+//     },
+//     [onFinish, preventDefault]
+//   );
+
+//   const move = useCallback(
+//     (event: MouseEvent | TouchEvent) => {
+//       if (cancelOnMove) {
+//         const dx = Math.abs(
+//           event instanceof MouseEvent
+//             ? event.clientX - (startX.current as number)
+//             : event.touches[0].clientX - (startX.current as number)
+//         );
+//         const dy = Math.abs(
+//           event instanceof MouseEvent
+//             ? event.clientY - (startY.current as number)
+//             : event.touches[0].clientY - (startY.current as number)
+//         );
+//         if (dx > 10 || dy > 10) {
+//           stop(event, false);
+//         }
+//       }
+//     },
+//     [cancelOnMove, stop]
+//   );
+
+//   const handlers = {
+//     onMouseDown: start,
+//     onTouchStart: start,
+//     onMouseUp: (event: MouseEvent) => stop(event),
+//     onTouchEnd: (event: TouchEvent) => stop(event),
+//     onTouchMove: move,
+//     onMouseMove: move,
+//   };
+
+//   return handlers;
+// };
+
+// export { useLongPress };
+
 import { useCallback, useRef } from "react";
 
 interface LongPressOptions {
@@ -21,7 +122,7 @@ const useLongPress = (
     stopPropagation = false,
   }: LongPressOptions = {}
 ) => {
-  const timerRef = useRef<any>(null);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
   const startX = useRef<number | null>(null);
   const startY = useRef<number | null>(null);
 
@@ -36,10 +137,14 @@ const useLongPress = (
         event.stopPropagation();
       }
 
-      startX.current =
-        event instanceof MouseEvent ? event.clientX : event.touches[0].clientX;
-      startY.current =
-        event instanceof MouseEvent ? event.clientY : event.touches[0].clientY;
+      if (event instanceof MouseEvent) {
+        startX.current = event.clientX;
+        startY.current = event.clientY;
+      } else if (event.touches && event.touches.length > 0) {
+        startX.current = event.touches[0].clientX;
+        startY.current = event.touches[0].clientY;
+      }
+
       onStart(event);
 
       timerRef.current = setTimeout(() => {
@@ -68,16 +173,17 @@ const useLongPress = (
   const move = useCallback(
     (event: MouseEvent | TouchEvent) => {
       if (cancelOnMove) {
-        const dx = Math.abs(
-          event instanceof MouseEvent
-            ? event.clientX - (startX.current as number)
-            : event.touches[0].clientX - (startX.current as number)
-        );
-        const dy = Math.abs(
-          event instanceof MouseEvent
-            ? event.clientY - (startY.current as number)
-            : event.touches[0].clientY - (startY.current as number)
-        );
+        let dx = 0;
+        let dy = 0;
+
+        if (event instanceof MouseEvent) {
+          dx = Math.abs(event.clientX - (startX.current as number));
+          dy = Math.abs(event.clientY - (startY.current as number));
+        } else if (event.touches && event.touches.length > 0) {
+          dx = Math.abs(event.touches[0].clientX - (startX.current as number));
+          dy = Math.abs(event.touches[0].clientY - (startY.current as number));
+        }
+
         if (dx > 10 || dy > 10) {
           stop(event, false);
         }
@@ -98,4 +204,4 @@ const useLongPress = (
   return handlers;
 };
 
-export  {useLongPress};
+export { useLongPress };
